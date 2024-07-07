9a0f9587ed4c4ea6c66b100cd34e339aa39fb635
IgorKopitsa <igor.kopitsa68@yandex.ru>
2024-07-07 16:08:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+print("1st program")
+print(9 ** 0.5*5)
+print("2nd program")
+print(9.99>9.98 and 1000 != 1000.1) # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("3rd program")
+a= 1234
+middle = a % 1000 // 10
+print(middle)
+a=5678
+middle = a % 1000 // 10
+print(middle)
+a = 23
+b = 67
+print(a+b)
+print("4th program")
+a=13.42
+b=42.13
+print(42==42, "13.42==42.13")
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
